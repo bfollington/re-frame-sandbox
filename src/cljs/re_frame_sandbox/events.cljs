c550a65e56e140c17f089b07b8a10934f9e9b9b5
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-09-08 17:13:37 +1000

    Shorten imports

--- src/cljs/re_frame_sandbox/events.cljs
+++ src/cljs/re_frame_sandbox/events.cljs
@@ -1,23 +1,23 @@
 (ns re-frame-sandbox.events
   (:require
-   [re-frame.core :as re-frame]
+   [re-frame.core :as rf]
    [re-frame-sandbox.db :as db]
    [day8.re-frame.tracing :refer-macros [fn-traced]]
    ))
 
-(re-frame/reg-event-db
+(rf/reg-event-db
  ::initialize-db
  (fn-traced [_ _]
    db/default-db))
 
-(re-frame/reg-event-db
+(rf/reg-event-db
   :change-name
   (fn-traced [db [_ name]]
     (-> db
       (assoc :name name)
       (assoc :name-field ""))))
 
-(re-frame/reg-event-db
+(rf/reg-event-db
   :change-name-field
   (fn-traced [db [_ name]]
     (assoc db :name-field name)))
--- src/cljs/re_frame_sandbox/subs.cljs
+++ src/cljs/re_frame_sandbox/subs.cljs
@@ -1,13 +1,13 @@
 (ns re-frame-sandbox.subs
   (:require
-   [re-frame.core :as re-frame]))
+   [re-frame.core :as rf]))
 
-(re-frame/reg-sub
+(rf/reg-sub
  ::name
  (fn [db]
    (:name db)))
 
-(re-frame/reg-sub
+(rf/reg-sub
  ::name-field
  (fn [db]
    (:name-field db)))
